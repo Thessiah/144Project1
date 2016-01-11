e9e2be739758f0cbb47a963afa982485ee1b8d6d
David Yang <davidyang6us@gmail.com>
2016-01-11 01:07:04 -0800

    Filled out a little more to make myself feel good

---
+++ ComputeSHA.java
@@ -0,0 +1,9 @@
+public class ComputeSHA {
+	public ComputeSHA () {
+		
+	}
+	
+	public static void main (String[] args) {
+		
+	}
+}
--- actors.sql
+++ actors.sql
@@ -1,3 +1,6 @@
 CREATE TABLE Actors(Name char(40), Movie char(80), Year int, Role char(40));
 
-LOAD DATA LOCAL INFILE ~/data/actors.csv INTO TABLE Actors;
+LOAD DATA LOCAL INFILE ~/data/actors.csv INTO TABLE Actors
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';;
+
+UPDATE Actors SET Movie = TRIM(Movie);
